7675f6d2825de3d3561e1f3ccdc11f1e7a5c7f9f
Tanayp <tanayp@Tanayp.local>
2022-01-12 10:45:38 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/tanaypatankar/go-workspace/dbtests
+
+go 1.17
+
+require (
+	gorm.io/driver/mysql v1.2.3
+	gorm.io/gorm v1.22.4
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
---
+++ routes/userrouter.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/tanaypatankar/go-workspace/dbtests/controller"
+)
+
+func InitializeRouter() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users/", controller.GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", controller.GetUserByID).Methods("GET")
+	// r.HandleFunc("/users/", controller.CreateUser).Methods("POST")
+	// r.HandleFunc("/users/{userid}", controller.UpdateUser).Methods("PUT")
+	// r.HandleFunc("/users/{userid}", controller.DeleteUser).Methods("DELETE")
+
+	http.Handle("/", r)
+	http.ListenAndServe("localhost:9010", nil)
+}
