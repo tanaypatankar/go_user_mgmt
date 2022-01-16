4ca38be624a8a388100869bb341fa3932899003a
joey00072 <shubhamchoudhari00072@gmail.com>
2022-01-16 15:06:46 +0530

    :sparkles: Added create,update & delete endpoint

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/tanaypatankar/go_user_mgmt/database"
+	"github.com/tanaypatankar/go_user_mgmt/models"
 	"github.com/tanaypatankar/go_user_mgmt/routes"
 )
 
@@ -18,18 +19,18 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 	database.DB_Init()
-	// user := models.User{
-	// 	Id:      1,
-	// 	Name:    "Tanay",
-	// 	Email:   "tanay@platform9.com",
-	// 	Age:     21,
-	// 	Gender:  "M",
-	// 	Country: "India",
-	// 	Status:  "Active",
-	// }
-	// result := database.DB.Create(&user)
-	// fmt.Println(result.Error)
-	// fmt.Println(result.RowsAffected)
+	user := models.User{
+		Id:      1,
+		Name:    "Tanay",
+		Email:   "tanay@platform9.com",
+		Age:     21,
+		Gender:  "M",
+		Country: "India",
+		Status:  "Active",
+	}
+	result := database.DB.Create(&user)
+	fmt.Println(result.Error)
+	fmt.Println(result.RowsAffected)
 
 	routes.InitializeRouter()
 
