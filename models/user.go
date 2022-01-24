8b9bf1ab2aee206ac67400a179ab384956cbaaab
Tanayp <tanayp@Tanayp.local>
2022-01-25 00:00:58 +0530

    misc updates

--- models/user.go
+++ models/user.go
@@ -1,16 +1,11 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 type User struct {
-	gorm.Model `json:"-"`
-	Id         int    `json:"id" gorm:"primaryKey"`
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	Age        int    `json:"age"`
-	Gender     string `json:"gender"`
-	Country    string `json:"country"`
-	Status     string `json:"status"`
+	Id      int    `json:"id" gorm:"primaryKey"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Age     int    `json:"age"`
+	Gender  string `json:"gender"`
+	Country string `json:"country"`
+	Status  string `json:"status"`
 }
